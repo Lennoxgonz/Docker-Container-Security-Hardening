55659794959fa0c63509c001b8c112014044fabe
Lennoxgonz <lennoxgonz@gmail.com>
2025-07-31 17:59:23 +0000

    added number and special char req for sign up in PERN app

--- PERN-app/vulnerable/frontend/src/pages/SignUpPage.tsx
+++ PERN-app/vulnerable/frontend/src/pages/SignUpPage.tsx
@@ -14,6 +14,8 @@ const signUpSchema = z
       ),
     password: z
       .string()
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[^a-zA-Z0-9]/, "Password must contain at least one special character")
       .min(8, "Password must be at least 8 characters long"),
     confirmPassword: z.string(),
   })
